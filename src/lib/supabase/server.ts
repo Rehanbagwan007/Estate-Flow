2f248c210d0048cd5b60e50fdf43bb2e53829c74
Rehan Bagwan <rehanbagwan10000@gmail.com>
2025-10-22 06:06:57 +0000

    ReferenceError: value is not defined     at Object.remove (file:///home/

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import type { Database } from '@/lib/types'
 
-export async function createClient() {
+export function createClient() {
   const cookieStore = cookies()
 
   return createServerClient<Database>(
@@ -10,23 +10,21 @@ export async function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
         cookies: {
-        async get(name: string) {
-         
-          let cookieName= (await cookieStore).get(name)?.value
-          return cookieName
+        get(name: string) {
+          return cookieStore.get(name)?.value
         },
-      async  set(name: string, value: string, options: CookieOptions) {
+      set(name: string, value: string, options: CookieOptions) {
           try {
-            (await cookieStore).set({ name, value, ...options })
+            cookieStore.set({ name, value, ...options })
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
-        async remove(name: string, options: CookieOptions) {
+        remove(name: string, options: CookieOptions) {
           try {
-            (await cookieStore).set({ name, value: '', ...options })
+            cookieStore.set({ name, value: '', ...options })
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
